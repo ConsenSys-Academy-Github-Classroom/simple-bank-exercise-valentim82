712f1dc217286ba9f231c4d52859df31050e5927
valentim82 <ubuntu@ip-172-31-20-251.us-east-2.compute.internal>
2021-10-03 21:30:01 +0000

    SimpleBank first tentative

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -14,7 +14,7 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) balances ;
+    mapping (address => uint) internal balances ;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
@@ -32,11 +32,11 @@ contract SimpleBank {
     event LogEnrolled(address accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     /* Functions
      */
@@ -52,10 +52,13 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[owner];
+	     
     }
 
     /// @notice Enroll a customer with the bank
@@ -70,7 +73,7 @@ contract SimpleBank {
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
@@ -81,23 +84,47 @@ contract SimpleBank {
       // 4. Emit the appropriate event associated with this function
 
       // 5. return the balance of sndr of this transaction
+	   
+	   if(enrolled[msg.sender]){
+		  balances[owner] += msg.value;
+		  // 4. Emit the appropriate event associated with this function 
+	  	emit LogDepositMade(msg.sender, msg.value);
+		  return balances[owner];	  
+	  }
+	  
+	  return balances[owner];
     }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(uint withdrawAmount) public payable returns (uint) {
       // If the sender's balance is at least the amount they want to withdraw,
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
       // return the user's balance.
 
       // 1. Use a require expression to guard/ensure sender has enough funds
-
+      require (withdrawAmount>0);
+      require(balances[msg.sender] >= withdrawAmount);
+      //require(enrolled[msg.sender]==true);
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
+      balances[msg.sender] -= withdrawAmount;
+      if(!msg.sender.send(withdrawAmount)){
 
-      // 3. Emit the appropriate event for this message
+        balances[msg.sender] += withdrawAmount;  
+      }
+      else{
+         // 3. Emit the appropriate event for this message
+      
+        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+
+      }
+     
+
+      return balances[msg.sender];
+
     }
 }
